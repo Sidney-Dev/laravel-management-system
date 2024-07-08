240a05391287f4e6f65ef7923e62106bdba8ed95
Elton Sidney Pedro de Sousa <sidneymelodia@Eltons-MacBook-Pro.local>
2024-07-08 17:31:13 +0200

    frontend validation handling

--- resources/views/dashboard/projects/create-project.blade.php
+++ resources/views/dashboard/projects/create-project.blade.php
@@ -13,15 +13,24 @@
 
             <div class="mt-2">
                 <x-forms.input-group id="name" label="Project Name" name="name" type="text" placeholder="Name of the project" />
+                @error('name')
+                    <p class="mt-2 text-sm text-red-500">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mt-4">
                 <div class="flex gap-1 w-full">
                     <div class="w-1/2">
                         <x-forms.input-group id="start_date" label="Project Start Date" name="start_date" type="date" />
+                        @error('start_date')
+                            <p class="mt-2 text-sm text-red-500">{{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="w-1/2">
                         <x-forms.input-group id="end_date" label="Project End Date" name="end_date" type="date" />
+                        @error('end_date')
+                            <p class="mt-2 text-sm text-red-500">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -31,6 +40,9 @@
                 <x-forms.input-group id="low" label="Low" name="priority" type="radio" value="low" />
                 <x-forms.input-group id="medium" label="Medium" name="priority" type="radio" value="medium" />
                 <x-forms.input-group id="high" label="High" name="priority" type="radio" value="high" />
+                @error('priority')
+                    <p class="mt-2 text-sm text-red-500">{{ $message }}</p>
+                @enderror
             </div>
 
             <x-forms.input-group 
